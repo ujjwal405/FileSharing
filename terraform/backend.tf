8301a4f942f95fd66e657460f6d44ed87e3f7d3c
ujjwal405 <silwalujjwal3@gmail.com>
2025-05-14 11:27:49 +0545

    removed if condition

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "filesharing-terraform-backend"
-    key          = "filesharing-backend/terraform.tfstate"
+    key          = "prod/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true #S3 native locking
