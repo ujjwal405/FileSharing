36f00b98a0b8e81f6afd91ae844c3a322d9e45a3
ujjwal405 <silwalujjwal3@gmail.com>
2025-05-14 17:23:17 +0545

    removed v in version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "v5.97.0"
+      version = "5.97.0"
     }
     archive = {
       source = "hashicorp/archive"
