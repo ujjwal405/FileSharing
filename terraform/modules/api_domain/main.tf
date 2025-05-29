b07200bda30a1c1a41c0343b59b3746d33b1d321
ujjwal405 <silwalujjwal3@gmail.com>
2025-05-29 17:50:40 +0545

    api_domain module added

--- terraform/modules/api_domain/main.tf
+++ terraform/modules/api_domain/main.tf
@@ -1,10 +1,16 @@
+data "aws_acm_certificate" "cert" {
+  domain      = var.domain_name # Replace with your domain
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
+
 resource "aws_api_gateway_domain_name" "custom_domain" {
-  domain_name              = "api.example.com"                                                         # Replace with your custom domain
-  regional_certificate_arn = "arn:aws:acm:us-east-1:123456789012:certificate/your-acm-certificate-arn" # Replace with ACM certificate ARN
+  domain_name              = var.domain_name                   # Replace with your custom domain
+  regional_certificate_arn = data.aws_acm_certificate.cert.arn # Replace with ACM certificate ARN
 }
 
 resource "aws_api_gateway_base_path_mapping" "custom_domain_mapping" {
   domain_name = aws_api_gateway_domain_name.custom_domain.id
-  rest_api_id = var.rest_api_id
+  api_id      = var.rest_api_id
   stage_name  = var.stage_name # Replace with your API stage name
 }
