61b3f149bdf7ee467b0ad8c04f98f00525f3e6e7
ujjwal405 <silwalujjwal3@gmail.com>
2025-07-25 11:55:29 +0545

    removed resources variable

--- terraform/modules/api_resource/variables.tf
+++ terraform/modules/api_resource/variables.tf
@@ -1,11 +1,13 @@
 variable "rest_api_id" {}
 
-variable "resources" {
-  type = list(object({
-    name      = string
-    parent_id = string
-  }))
-}
+# variable "resources" {
+#   type = list(object({
+#     name      = string
+#     parent_id = string
+#   }))
+# }
 
+variable "parent_id" {}
+variable "path_part" {}
 
 
